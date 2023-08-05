7f09b7f401f74da9b5baf7aa07853e111ce2c194
Christopherr01 <ani.chris001@gmail.com>
2023-08-05 16:32:40 +0700

    uploading code

---
+++ walletapp/main.py
@@ -0,0 +1,9 @@
+import walletapp.app as app
+
+def run():
+    archon_name = 'Barbatos'
+    print(app.q01(archon_name))
+
+# Call the run() function to execute the code
+if __name__ == '__main__':
+    run()
